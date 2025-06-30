1d7a0b87250bd0a2920cf80bc3963f31f8cfbc07
jyothish <jyothish28082004@gmail.com>
2025-06-30 22:15:05 +0530

    intial

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import autRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { app, server } from './lib/socket.js';
+
+import path from 'path';
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, // Allow cookies to be sent
+}));
+
+
+app.use("/api/auth", autRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  });
+} 
+
+server.listen(PORT, () => {
+  console.log('Server is running on http://localhost:' + PORT);
+  connectDB()
+});
