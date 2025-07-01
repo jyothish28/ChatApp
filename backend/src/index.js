b286cdc89fbc7d334fe031eb4e93b9b692bc81b3
jyothish28 <157520398+jyothish28@users.noreply.github.com>
2025-07-01 18:42:05 +0530

    Update index.js1

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
- origin: process.env.CLIENT_URL || "http://localhost:5173",
+ origin: "https://chatapp-frontend-gep4.onrender.com",
   credentials: true, // Allow cookies to be sent
 }));
 
