352fdbfd8264b12b5b0e8809df796003b2652ff4
jyothish28 <157520398+jyothish28@users.noreply.github.com>
2025-07-01 18:00:55 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
 
-import path from 'path';
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,13 +24,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-  });
-} 
+
 
 server.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
