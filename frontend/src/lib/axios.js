fd734c4c273796083c3106422374e35c7c691bd1
jyothish28 <157520398+jyothish28@users.noreply.github.com>
2025-07-01 18:20:53 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-1-d7fi.onrender.com/api",
+    baseURL: "https://chatapp-1-d7fi.onrender.com/api",
     withCredentials: true,
 });
